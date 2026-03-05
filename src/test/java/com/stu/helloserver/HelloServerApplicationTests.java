ed850dbae3446f69756cf1df3e086992e4ff8b58
zlf <2428637414@qq.com>
2026-03-05 15:10:13 +0800

    1

---
+++ src/test/java/com/stu/helloserver/HelloServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stu.helloserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
